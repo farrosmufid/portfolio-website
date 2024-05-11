0c20291c659b057e705c0d460bef4c078033100e
farros <31735132+farrosmufid@users.noreply.github.com>
2024-05-12 00:34:25 +1000

    Created Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <li className="nav--logo_text">
+                    <a href="#home--hero_section">Farros Mufid</a>
+                </li>
+                <li>
+                    <a href="#my_timeline_section">My Timeline</a>
+                </li>
+                <li>
+                    <a href="#design_work_section">Design Work</a>
+                </li>
+                <li>
+                    <a href="#awards_section">Awards</a>
+                </li>
+                <li>
+                    <a href="#contact_me_section">Contact Me</a>
+                </li>
+            </nav>
+        </header>
+
+    ) 
+}
