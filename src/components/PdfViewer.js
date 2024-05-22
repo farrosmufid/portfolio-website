bac889a3977e792532d5efb75337046ef16f5bc1
farros <31735132+farrosmufid@users.noreply.github.com>
2024-05-22 11:10:09 +1000

    Fixed accessibility issue by adding title to iframe for screen readers

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PdfViewer = () => {
     return (
         <div className="iframe-container">
-            <iframe src="/files/Group_12_Presentation.pdf" width="100%">
+            <iframe src="/files/Group_12_Presentation.pdf" width="100%" title="Group 12 Presentation PDF Viewer">
                 This browser does not support PDFs. Please download the PDF to view it: <a href="/files/Group_12_Presentation.pdf">Download PDF</a>.
             </iframe>
         </div>
