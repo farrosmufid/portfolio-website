06b229235fd9c866a7f9d8758ad6452095deec0f
farros <31735132+farrosmufid@users.noreply.github.com>
2025-12-09 18:09:55 +1100

    Responsive Design

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -6,11 +6,14 @@ export default function HomeHero({roles, currentIndex}) {
             <div className="home--hero_content">
                 <div className="home--hero_left">
                     <img src="/images/profile_pictures/profile_picture_1.jpeg" alt="Profile" className="home--hero_profile_picture" />
+                    <h5 className="home--hero_text_header home--hero_text_header--mobile">Hello, I am a {" "}
+                        <span className="home--hero_text_span">{roles[currentIndex]}</span>
+                    </h5>
                     <p className="home--hero_description">
                         I'm a <strong>Software Engineer</strong> with <strong>1 year of experience</strong> building and architecting software for <strong>Braiv</strong> and <strong>Accenture</strong>. At Braiv, I <strong>architect ML pipelines</strong> and <strong>implement cloud engineering</strong> and <strong>DevOps practices</strong>. At Accenture, I <strong>engineered ETL data pipelines</strong>. I'm also an <strong>electronics enthusiast</strong> and currently work as a <strong>Retail Sales Assistant</strong> at <strong>Jaycar</strong> (6+ months of retail experience).
                     </p>
                 </div>
-                <h5 className="home--hero_text_header">Hello, I am a {" "}
+                <h5 className="home--hero_text_header home--hero_text_header--desktop">Hello, I am a {" "}
                     <span className="home--hero_text_span">{roles[currentIndex]}</span>
                 </h5>
             </div>
